1d345702d3ab0ff3b7a017531012a0505aa489c3
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-02-14 18:18:34 -0300

    moved some code to Letters component

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,11 @@ import palavras from "../palavras"
 
 
 const App = () => {
-  const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
   return (
     <main className="App">
       <Game />
-      <div className="container">
-        <div className="letters">
-          {alfabeto.map(letter => { 
-            return <button key={letter} disabled={true}>{letter.toUpperCase()}</button>
-          })}
-        </div>
-      </div>
+      <Letters />
     </main>
   );
 }
--- src/components/Letters.js
+++ src/components/Letters.js
@@ -1,7 +1,12 @@
 const Letters = () => {
+    const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     return (
-        <div>
-            
+        <div className="container">
+            <div className="letters">
+            {alfabeto.map(letter => { 
+                return <button key={letter} disabled={true}>{letter.toUpperCase()}</button>
+            })}
+            </div>
         </div>
     );
 };
