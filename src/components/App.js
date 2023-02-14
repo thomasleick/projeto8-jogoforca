ce7a1b79d28a220cd8037a87cbce66f4073ea0ac
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-02-14 18:01:43 -0300

    moved some code to Game component

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,14 @@
+import Game from "./Game";
+import Letters from "./Letters";
 import palavras from "../palavras"
+
 
 const App = () => {
   const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
   return (
     <main className="App">
-      <div className="container top">
-        <div className="left">
-          <img src="../assets/forca0.png" />
-        </div>
-        <div className="right">
-          <button >Escolher Palavra</button>
-        </div>
-      </div>
+      <Game />
       <div className="container">
         <div className="letters">
           {alfabeto.map(letter => { 
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,12 @@
 const Game = () => {
     return (
-        <div>
-            
+        <div className="container top">
+            <div className="left">
+            <img src="../assets/forca0.png" />
+            </div>
+            <div className="right">
+            <button >Escolher Palavra</button>
+            </div>
         </div>
     );
 };
