df50edb94c73e6d9ccd77ca94c7c20416dfe878f
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-02-16 09:42:05 -0300

    data-test guess-button

--- src/components/Guess.js
+++ src/components/Guess.js
@@ -6,7 +6,7 @@ const Guess = (params) => {
         <div className="guess">
             <h2>Já sei a palavra!</h2>
             <input onChange={e => setGuess(e.target.value)} value={guess}></input>
-            <button onClick={() => handleGuessButton()} disabled={disabled}>Chutar</button>
+            <button onClick={() => handleGuessButton()} disabled={disabled} data-test="guess-button">Chutar</button>
         </div>
     );
 };
