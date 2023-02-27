4e10ab3c97c286ca1aae69e8b6c747fa26922bfe
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-02-27 16:12:47 -0300

    Letters Component with styled-components

--- src/components/Letters.js
+++ src/components/Letters.js
@@ -1,25 +1,59 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly; 
+  width: 100%;
+`
+
+const LettersDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  max-width: 676px;
+`
+const Button = styled.button`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 19px;
+  text-align: center;
+  width: 40px;
+  height: 40px;
+  color: #39739D;
+  background-color: #E1ECF4;
+  border: 1px solid #7AA7C7;
+  border-radius: 3px;
+  margin: 6px 6px;
+  &:disabled{background-color: #798A9F;}
+  
+`
 const Letters = (props) => {
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     const { disabled, chosenLetters, handleButtonClick } = props;
   
     return (
-      <div className="container">
-        <div className="letters">
+      <Container>
+        <LettersDiv>
           {alfabeto.map((letter) => {
             const isChosen = chosenLetters.includes(letter);
             return (
-              <button
+              <Button
                 key={letter}
                 onClick={() => handleButtonClick(letter)}
                 disabled={disabled || isChosen}
                 data-test="letter"
               >
                 {letter.toUpperCase()}
-              </button>
+              </Button>
             );
           })}
-        </div>
-      </div>
+        </LettersDiv>
+      </Container>
     );
   };
 
